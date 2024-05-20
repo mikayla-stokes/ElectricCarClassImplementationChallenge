c13feb7a322b6eb29fc9a6ccf3255cd2f5e69f6b
mikayla-stokes <mikaylastokes@usf.edu>
2024-05-20 14:41:35 -0400

    Create EV class extending Car with additional charge property

---
+++ cc_4.js
@@ -0,0 +1,37 @@
+//Constructor that initializes make and speed (current speed in km/h)
+class Car {
+    constructor (make,speed) {
+        this.make=make;
+        this.speed=speed;
+    }
+//accelerate method
+    accelerate () {
+        this.speed += 10;
+        console.log(`New speed:${this.speed} km/h`);
+    }
+//brake method
+    brake () {
+        this.speed -= 5;
+        console.log(`New speed:${this.speed} km/h`);
+    }
+}
+
+class EV extends Car {
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.charge = charge;
+    }
+}
+
+//Example
+Car1 = new Car('BMW', 120);
+Car2 = new Car('Mercedes', 95);
+
+//Test data
+Car1.accelerate();
+Car1.brake();
+Car1.brake();
+
+Car2.accelerate();
+Car2.accelerate();
+Car2.brake();
